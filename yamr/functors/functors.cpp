e5677d41e8258533787d43a1a3ae44851536ae73
Oleg M <moleg_9432@rambler.ru>
2024-08-26 01:27:12 +0300

    in dev

--- yamr/functors/functors.cpp
+++ yamr/functors/functors.cpp
@@ -4,8 +4,9 @@
 
 void MapFunctor::add_line(const std::string& line)
 {
-    for (std::size_t index = 1; index <= line.length(); ++index)
+    for (std::size_t index = 1; index <= line.length(); ++index) {
         map_lines.push_back(line.substr(0, index));
+    }
 }
 
 std::vector<std::string> MapFunctor::operator() () const
@@ -13,21 +14,19 @@ std::vector<std::string> MapFunctor::operator() () const
     return map_lines;
 }
 
+
 std::size_t ReduceFunctor::operator() (const std::multiset<std::string>& lines)
 {
-    std::size_t maxLength = 0;
+    std::size_t max_length = 0;
     
-    for (const std::string& line : lines)
-    {
-
+    for (const std::string& line : lines) {
         int size = ++counters[line];
-        if (size > 1 && line.length() > maxLength)
-        {
 
-            maxLength = line.length();
+        if (size > 1 && line.length() > max_length) {
+            max_length = line.length();
         }
     }
-    return maxLength + 1;
+    return max_length + 1;
 }
 
 
